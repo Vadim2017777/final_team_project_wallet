0ff668bba8d68ea272c60b8cea96402b75998bd6
Vadim <svanovivan@gmail.com.com>
2020-10-29 19:22:28 +0200

    Add new App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import {BrowserRouter} from 'react-router-dom';
 
+import './App.css';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RegisterForm />
+    </BrowserRouter>
+  );
+}
+
+export default App;
