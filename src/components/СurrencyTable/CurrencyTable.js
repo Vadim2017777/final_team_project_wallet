92f2e978c6b7fbf39fffaca5bb7ab8d4bd6a384f
Vadim <svanovivan@gmail.com>
2020-11-09 21:45:07 +0200

    Add font to logo_login logo_register,

--- src/components/СurrencyTable/CurrencyTable.js
+++ src/components/СurrencyTable/CurrencyTable.js
@@ -11,7 +11,7 @@ import Skeleton from 'react-loading-skeleton';
 import s from './CurrencyTable.module.css';
 
 const CurrencyTable = ({currency, getCurrency}) => {
-  console.log('----------->', currency);
+  console.log('----------->', typeof currency);
   const [USD, EUR, RUB] = currency;
   useEffect(() => {
     getCurrency();
